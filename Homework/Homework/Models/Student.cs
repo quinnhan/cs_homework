e8b3e7a74e35428362c9c6214a1b7143908534b5
cmqhuy <cmqhuy@gmail.com>
2017-05-29 23:48:18 -0700

    Add exercise

---
+++ Homework/Homework/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace Homework.Models
+{
+    public class Student : ObservableObject
+    {
+        private string name;
+        private int age;
+        private Gender gender;
+
+        public string Name { get { return name; } set { SetProperty(ref name, value); } }
+        public int Age { get { return age; } set { SetProperty(ref age, value); } }
+        public Gender Gender { get { return gender; } set { SetProperty(ref gender, value); } }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
---
+++ Homework/Homework/ViewModels/MainPageVM.cs
@@ -0,0 +1,48 @@
+﻿using Homework.Models;
+using System.Collections.ObjectModel;
+
+namespace Homework.ViewModels
+{
+    public class MainPageVM : ObservableObject
+    {
+        private ObservableCollection<Student> studentList;
+        public ObservableCollection<Student> StudentList { get { return studentList; } set { SetProperty(ref studentList, value); } }
+
+        private string newStudentName;
+        private string newStudentAge;
+        private bool newStudentIsMale;
+
+        public string NewStudentName { get { return newStudentName; } set { SetProperty(ref newStudentName, value); } }
+        public string NewStudentAge { get { return newStudentAge; } set { SetProperty(ref newStudentAge, value); } }
+        public bool NewStudentIsMale { get { return newStudentIsMale; } set { SetProperty(ref newStudentIsMale, value); } }
+
+        public MainPageVM()
+        {
+            StudentList = new ObservableCollection<Student>();
+        }
+
+        public void AddNewStudent()
+        {
+            var name = this.NewStudentName;
+
+            if (string.IsNullOrEmpty(name))
+                return;
+
+            if (!int.TryParse(this.NewStudentAge, out int age))
+                return;
+
+            var gender = this.NewStudentIsMale ? Gender.Male : Gender.Female;
+
+            this.StudentList.Add(new Student
+            {
+                Name = name,
+                Age = age,
+                Gender = gender
+            });
+
+            this.NewStudentName = string.Empty;
+            this.NewStudentAge = string.Empty;
+            this.NewStudentIsMale = false;
+        }
+    }
+}
