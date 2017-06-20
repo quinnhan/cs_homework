6007dc12ac69f114cf32de5c07b694264a875026
quinnhan <hdang8792@gmail.com>
2017-06-20 15:13:38 -0700

    StudentList

---
+++ StudentList/StudentList/Models/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using StudentList.ViewModels;
+using System.Threading.Tasks;
+
+namespace StudentList.Models
+{
+    class Person : ObservableObject
+    {
+        private string name;
+        private int age;
+        private Gender _gender;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                RaisePropertyChangedEvent("Name");
+            }
+        }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                age = value;
+                RaisePropertyChangedEvent("Age");
+            }
+        }
+        public Gender gender
+        {
+            get { return _gender; }
+            set
+            {
+                _gender = value;
+                RaisePropertyChangedEvent("gender");
+            }
+        }
+
+        public enum Gender
+        {
+            Male,
+            Female
+        }
+        
+    }
+}
---
+++ StudentList/StudentList/ViewModels/Presenter.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Windows.Input;
+using StudentList.ViewModels;
+using StudentList.Models;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentList
+{
+    class Presenter : ObservableObject
+    {
+        private ICommand ButtonCommand;
+        private ICommand AddButtonCommand
+        {
+            get { return ButtonCommand; }
+            set { ButtonCommand = value; }
+        }
+
+        private ObservableCollection<Person> studentList;
+        public ObservableCollection<Person> StudentList
+        {
+            get{ return studentList;}
+            set
+            {
+                studentList = value;
+                RaisePropertyChangedEvent("StudentList");
+            }
+        }
+
+        private string newPersonName;
+        private string newAge;
+        private bool newIsMale;
+
+        public string NewPersonName
+        {
+            get { return newPersonName;}
+            set
+            {
+                newPersonName = value;
+                RaisePropertyChangedEvent("NewPersonName");
+            }
+        }
+
+        public string NewAge
+        {
+            get { return newAge; }
+            set
+            {
+                newAge = value;
+                RaisePropertyChangedEvent("NewAge");
+            }
+        }
+
+        public bool NewIsMale
+        {
+            get { return newIsMale; }
+            set
+            {
+                newIsMale = value;
+                RaisePropertyChangedEvent("NewIsMale");
+            }
+        }
+
+        public Presenter()
+        {
+            StudentList = new ObservableCollection<Person>();
+        }
+
+        public void AddNewStudent()
+        {
+            var name = this.NewPersonName;
+
+            if (string.IsNullOrEmpty(name))
+                return;
+
+            if (!int.TryParse(this.NewAge, out int age))
+                return;
+
+            var gender = this.NewIsMale ? Person.Gender.Male : Person.Gender.Female;
+
+            this.StudentList.Add(new Person
+            {
+                Name = name,
+                Age = age,
+                gender = gender
+            });
+
+            this.NewPersonName = string.Empty;
+            this.NewAge = string.Empty;
+            this.NewIsMale = false;
+        }
+    }
+}
