ebb69f859abfd8cd8d4272d4a537959f22c90fd5
quinnhan <hdang8792@gmail.com>
2017-06-21 16:43:02 -0700

    StudentList

---
+++ Homework/StudentList/StudentList/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using StudentList.ViewModels;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace StudentList
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public Presenter ViewModel { get; private set; }
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            ViewModel = new Presenter();
+            this.DataContext = this.ViewModel;
+        }
+    }
+}
