dc411fee110b2393e92871c2ed1ca139a5e6cbdf
quinnhan <hdang8792@gmail.com>
2017-06-22 20:48:05 -0700

    message

---
+++ StudentList/StudentList/ViewModels/ObservableObject.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentList.ViewModels
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
